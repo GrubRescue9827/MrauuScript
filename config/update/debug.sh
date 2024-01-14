ac8e9d6b151392d83135eb28853b3c51a53f8734
GrubRescue9827 <147955729+GrubRescue9827@users.noreply.github.com>
2024-01-13 21:04:49 -0500

    Make it super obvious debug.sh is a placeholder

--- config/update/debug.sh
+++ config/update/debug.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+ #!/bin/bash
 
 # Debugging example. Useful if you want to skip updating the OS but don't want to use --skip_os_upgrade
-echo "[DEBUG] OS Update"
-sleep 5
+echo "###############################################"
+echo "#                                             #"
+echo "# [WARN] OS Update command is not configured! #"
+echo "#                                             #"
+echo "###############################################"
+echo
+for i in $(seq 5 -1 1);
+do
+    echo "[WARN] Continuing anyway in $i seconds... Press CTRL+C to abort."
+    sleep 1
+done
