f89d28706a1aa8c63f28788a0ef33b5d07329b50
GrubRescue9827 <147955729+GrubRescue9827@users.noreply.github.com>
2024-07-09 00:36:10 -0400

    Use new error system

--- config/startup/localtonet.sh
+++ config/startup/localtonet.sh
@@ -5,11 +5,11 @@ globals="/etc/opt/MrauuScript/globals.sh"
 if [[ -f $globals ]]; then
     source $globals
 else
-    echo "[FATAL][startup-localtonet] Config file not found: $globals"
+    $fatal "[FATAL][startup-localtonet] Config file not found: $globals"
     exit 1
 fi
 
 key=$(cat $AlertConf/ltn-auth)
 
 cd $bin/alerts/localtonet/
-sudo screen -dmS "localtonet" sudo -u $UnprivilegedUser ./localtonet authtoken $key || echo "[FATAL][startup-localtonet] Unhandled exception. Returned $?"
+sudo screen -dmS "localtonet" sudo -u $UnprivilegedUser ./localtonet authtoken $key || $fatal "[FATAL][startup-localtonet] Unhandled exception. Returned $?"
