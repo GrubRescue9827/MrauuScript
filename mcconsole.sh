d17d48f931d00def411d34c877f60b0bc5412e31
GrubRescue9827 <147955729+GrubRescue9827@users.noreply.github.com>
2024-07-09 00:23:19 -0400

    Add config check

--- mcconsole.sh
+++ mcconsole.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Check for global config file
+globals="/etc/opt/MrauuScript/globals.sh"
+if [[ -f $globals ]]; then
+    source $globals
+else
+    $fatal "[FATAL][mcupdate] Config file not found: $globals"
+    exit 1
+fi
+
 selection=$1
 
 case "$selection" in
   "" | "1" | "mc" | "m" | "M" | "minecraft")
-sudo screen -r "minecraft" || echo "[ERROR][mcconsole] Returned: $? (Is the server running?)";;
+sudo screen -r "minecraft" || $error "[ERROR][mcconsole] Returned: $? (Is the server running?)";;
   "2" | "ltn" | "l" | "L" | "localtonet")
-sudo screen -r "localtonet" || echo "[ERROR][mcconsole] Returned: $? (Is localtonet running?)";;
+sudo screen -r "localtonet" || $error "[ERROR][mcconsole] Returned: $? (Is localtonet running?)";;
   "3" | "ngk" | "n" | "N" | "ngrok")
-sudo screen -r "ngrok" || echo "[ERROR][mcconsole] Returned: $? (Is ngrok running?)";;
+sudo screen -r "ngrok" || $error "[ERROR][mcconsole] Returned: $? (Is ngrok running?)";;
   *)
-echo "[FATAL][mcconsole] Unknown selection \"$selection\"";;
+$fatal "[FATAL][mcconsole] Unknown selection \"$selection\"";;
 esac
