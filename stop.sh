065e9e92f526eba6de620f7e34baeb24f7082f1f
GrubRescue9827 <147955729+GrubRescue9827@users.noreply.github.com>
2024-07-09 00:21:02 -0400

    WIP: add wireguard support

--- stop.sh
+++ stop.sh
@@ -6,7 +6,7 @@ globals="/etc/opt/MrauuScript/globals.sh"
 if [[ -f $globals ]]; then
     source $globals
 else
-    echo "[FATAL][stop] Config file not found: $globals"
+    $fatal "[FATAL][stop] Config file not found: $globals"
     exit 1
 fi
 
@@ -17,9 +17,9 @@ function stopmc () {
 # Check if server is already stopped
 if pgrep -x "java" > /dev/null
 then
-    echo "[DEBUG][stop] Server is running."
+    $debug "[DEBUG][stop] Server is running."
 else
-    echo "[WARN][stop] Server is not running. Why are you attempting to stop it?"
+    $warn "[WARN][stop] Server is not running. Why are you attempting to stop it?"
     exit 1
 fi
 
@@ -29,11 +29,11 @@ else
     text=$reason
 fi
 
-echo "[INFO][stop] Shutting down with reason: $reason"
+$info "[INFO][stop] Shutting down with reason: $reason"
 
 # Forced shutdown
 if [ $force == "y" ]; then
-    echo "[INFO][stop] Flag force was called, shutting down immediately!"
+    $info "[INFO][stop] Flag force was called, shutting down immediately!"
     $srvmsg -m "stop" -e "y"
 fi
 
@@ -42,7 +42,7 @@ echo "[INFO][stop] Shutdown message sent to server chat."
 $srvmsg -m "$text"
 
 if [ $force == "n" ]; then
-    echo "[INFO][stop] Waiting for grace period to end..."
+    $info "[INFO][stop] Waiting for grace period to end..."
     sleep $graceperiod
 fi
 
@@ -50,7 +50,7 @@ fi
 if [ $enable_mercycountdown == y ]; then
     for s in {5..1}
     do
-        echo "[INFO][stop] Mercy countdown: $s"
+        $info "[INFO][stop] Mercy countdown: $s"
         text="Shutting down in $s seconds..."
         $srvmsg -m "$text"
         sleep 1
@@ -62,7 +62,7 @@ text="Goodbye~!"
 $srvmsg -m "$text"
 
 # Finally, actually stop the server.
-echo "[INFO][stop] Sending shutdown signal..."
+$info "[INFO][stop] Sending shutdown signal..."
 $srvmsg -m "stop" -e "y"
 
 # See if server has stopped yet. if not, warn user.
@@ -72,9 +72,9 @@ do
     do
         pgrep -x "java" > /dev/null || break
         if [ $s == 30 ]; then
-            echo "[ERROR][stop] Server did not respond to shutdown signal. (Is the shutdown command correct?)"
+            $error "[ERROR][stop] Server did not respond to shutdown signal. (Is the shutdown command correct?)"
         elif [ $s -gt 5 ]; then
-            echo "[WARN][stop] Server is taking a long time to stop! (Try $s of 30)"
+            $warn "[WARN][stop] Server is taking a long time to stop! (Try $s of 30)"
         fi
         sleep 1
     done
@@ -83,17 +83,21 @@ done
 }
 
 function stopltn () {
-    echo "[WARN] Forcefully killing localtonet!"
+    $warn "[WARN][stop] Forcefully killing localtonet!"
     sudo pkill localtonet
 }
 
 function stopngk () {
-    echo "[WARN] Forcefully killing ngrok!"
+    $warn "[WARN][stop] Forcefully killing ngrok!"
     sudo pkill ngrok
 }
 
 function stopts () {
     sudo tailscale down
+}
+
+function stopwg () {
+    sudo wg-quick down $(cat $StartupConf/wg-conf)
 }
 
 case "$tostop" in
@@ -102,11 +106,13 @@ case "$tostop" in
         stopltn
         stopngk
         stopts
+	stopwg
         ;;
     netonly)
         stopltn
         stopngk
         stopts
+	stopwg
         ;;
     mc)
         stopmc
@@ -120,7 +126,10 @@ case "$tostop" in
     ts)
         stopts
         ;;
+    wg)
+	stopwg
+	;;
     *)
-        echo "[ERROR] Invalid process name: $tostop"
+        $error "[ERROR][stop] Invalid process name: $tostop"
         ;;
 esac
