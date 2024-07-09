5672b968e7da2cfacd5d105f962796e09182d7ba
GrubRescue9827 <147955729+GrubRescue9827@users.noreply.github.com>
2024-07-09 00:23:49 -0400

    Add functionality: delete last x backups

--- mcbackup.sh
+++ mcbackup.sh
@@ -5,22 +5,30 @@ globals="/etc/opt/MrauuScript/globals.sh"
 if [[ -f $globals ]]; then
     source $globals
 else
-    echo "[FATAL][mcbackup] Config file not found: $globals"
+    $fatal "[FATAL][backup] Config file not found: $globals"
     exit 1
 fi
 
 export BARGS_VARS_PATH="$MrauuConfig/mcbackup"
 source $BargsLoc "$@"
 
-echo "[INFO][mcbackup] Begin backup..."
+# Sanity check
+if [[ "$BackupLoc" == "" ]]; then
+	$fatal "[FATAL][backup] Backup directory does not exist!"
+	exit 1
+fi
+
+# Inputs are OK
+$info "[INFO][backup] Begin backup..."
 
 # Check if running override-checks mode
+# TODO: figure out why we're doing this?
 if [ $skip_server_checks == force_yes ]; then
 	skip=1
 	else
 	# Prompt user in interactive mode.
 	if [ $skip_server_checks == y ]; then
-		echo "[WARN] Running with override in interactive mode."
+		$warn "[WARN][backup] Running with override in interactive mode."
 		echo "============================================================="
 		echo "|  You are running this script with --skip-server-checks    |"
 		echo "|  This override disables all sanity checks.                |"
@@ -31,10 +39,10 @@ if [ $skip_server_checks == force_yes ]; then
 		echo
 		read user_enabled_override
 		if [ "$user_enabled_override" == "Yes, do as I say!" ]; then
-			echo "[WARN] User chose to enable override."
+			$warn "[WARN][backup] User chose to enable override."
 			skip=1
 		else
-			echo "[FATAL] User aborted backup."
+			$fatal "[FATAL][backup] User aborted backup."
 			exit 1
 		fi
 	else
@@ -42,19 +50,45 @@ if [ $skip_server_checks == force_yes ]; then
 	fi
 fi
 
-if [ skip == 0 ]; then
+if [ "$skip" == "0" ]; then
 	# Check if the server is running
 	if pgrep -x "java" > /dev/null
 	then
-		echo "[WARN] Server is running, attempting shutdown!"
-		$stop -r "Shutting down for backup in "
+		$info "[INFO][backup] Server is running, attempting shutdown!"
+		$stop -r "Shutting down for backup in " -s mc
 		wasrunning=1
 	else
-		echo "[INFO] Server is not running, skipping shutdown."
+		$info "[INFO][backup] Server is not running, skipping shutdown."
 		wasrunning=0
 	fi
 fi
 
+# Delete last x backups
+if [[ "$delete_last" != "0" ]] || [[ "$BackupKeepLast" != "" ]]; then
+	$info "[INFO][backup] Cleaning up backups folder..."
+	cd "$BackupLoc"
+
+	# Count the number of files (excluding directories) in the directory
+	CurrentBackupCount=$(find . -maxdepth 1 -type f | wc -l)
+
+	# Calculate the number of backups to delete
+	let "BackupsToDelete = $CurrentBackupCount - $BackupKeepLast"
+
+	# Check if there are more than x files
+	if [ $BackupsToDelete -gt 0 ]; then
+	# Delete the oldest files
+	ls -tp | grep -v '/$' | tail -n "$BackupsToDelete" | xargs -I {} rm -v -- {}
+	else
+		$info "[INFO][backup] Backups folder does not need cleaning."
+	fi
+fi
+
+
 # Create backup folder if it doesn't exist
 #mkdir -p $BackupLoc
-echo tar -v -czf $BackupLoc/$BackupName $MCInstall
+tar -v -czf $BackupLoc/$BackupName $MCInstall
+
+if [[ "$wasrunning" == "1" ]]; then
+	$info "[INFO][backup] Rebooting server..."
+	$run -r mc
+fi
