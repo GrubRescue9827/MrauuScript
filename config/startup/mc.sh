aae2568d87295a63664e532cfcd3852611a6ea87
GrubRescue9827 <147955729+GrubRescue9827@users.noreply.github.com>
2024-07-09 00:36:39 -0400

    new error system + debug mode (no screen)

--- config/startup/mc.sh
+++ config/startup/mc.sh
@@ -5,10 +5,20 @@ globals="/etc/opt/MrauuScript/globals.sh"
 if [[ -f $globals ]]; then
     source $globals
 else
-    echo "[FATAL][startup-mc] Config file not found: $globals"
+    $fatal "[FATAL][startup-mc] Config file not found: $globals"
     exit 1
 fi
 
-cd $MCInstall
+if [[ "$1" == "--help" ]]; then
+    echo "Usage: mc.sh [OPTIONS]"
+    echo "    --debug        Prints all output to terminal instead of gnu/screen, allowing for easy scrollback. Useful for debugging crashes on startup!"
+    exit 0
+fi
 
-sudo screen -dmS "minecraft" $javastartup || echo "[FATAL][startup-mc] Unhandled exception. Returned $?"
+cd $MCInstall
+if [[ "$1" == "--debug" ]]; then
+    echo "[WARN][startup-mc] command line flag --debug was specified. Some functionality may not be available."
+    $javastartup
+else
+    sudo screen -dmS "minecraft" $javastartup || $fatal "[FATAL][startup-mc] Unhandled exception. Returned $?"
+fi
