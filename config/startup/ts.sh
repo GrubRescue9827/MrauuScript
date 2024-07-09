21f256bc09e95734b7d6d3df60dcb41d19443db3
GrubRescue9827 <147955729+GrubRescue9827@users.noreply.github.com>
2024-07-09 00:37:36 -0400

    Use new error system

--- config/startup/ts.sh
+++ config/startup/ts.sh
@@ -2,13 +2,13 @@
 
 if ! pgrep -x tailscaled > /dev/null
     then
-        echo "[WARN][startup-ts] Tailscale Daemon is not running! Attempting to enable with systemctl..."
+        $warn "[WARN][startup-ts] Tailscale Daemon is not running! Attempting to enable with systemctl..."
         systemctl enable tailscaled
         systemctl start tailscaled
 fi
 
 if [[ "$(tailscale status)" == *"Logged out."* ]]; then
-	echo "[FATAL][startup-ts] You are not logged into Tailscale!"
+	$fatal "[FATAL][startup-ts] You are not logged into Tailscale!"
 	exit 1
 else
 	tailscale up
