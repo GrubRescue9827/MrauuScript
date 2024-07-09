ce67f9f72406d18f8400bda72bd9d7203b71e45c
GrubRescue9827 <147955729+GrubRescue9827@users.noreply.github.com>
2024-07-09 00:34:20 -0400

    Genuinely the most atrocious script in this repo. I'm sorry.

--- config/alerts/watchdog.sh
+++ config/alerts/watchdog.sh
@@ -5,7 +5,7 @@ globals="/etc/opt/MrauuScript/globals.sh"
 if [[ -f $globals ]]; then
     source $globals
 else
-    echo "[FATAL][watchdog] Config file not found: $globals"
+    $fatal "[FATAL][watchdog] Config file not found: $globals"
     exit 1
 fi
 
@@ -13,11 +13,11 @@ ngktmp=$AlertConf/.ngk.tmp
 ltntmp=$AlertConf/.ltn.tmp
 
 if [[ ! -f $ngktmp ]]; then
-    echo "[WARN][watchdog] ngrok temp file was not found! Attempting to create..."
+    $warn "[WARN][watchdog] ngrok temp file was not found! Attempting to create..."
     touch $ngktmp
 fi
 if [[ ! -f $ltntmp ]]; then
-    echo "[WARN][watchdog] localtonet temp file was not found! Attempting to create..."
+    $warn "[WARN][watchdog] localtonet temp file was not found! Attempting to create..."
     touch $ltntmp
 fi
 
@@ -27,20 +27,22 @@ if [ ! "$ngk" == "" ]; then
     if [[ "$parsedngk" == *":"* ]]; then
         if [ ! "$(cat $ngktmp)" == "$parsedngk" ]; then
             echo $parsedngk > $ngktmp
-            echo "[INFO][watchdog] !! ngrok IP or port has changed !!"
+            $info "[INFO][watchdog] !! ngrok IP or port has changed !!"
             ngkchanged=1
         else
-            echo "[INFO][watchdog] ngrok IP or port has not changed."
+            $info "[INFO][watchdog] ngrok IP or port has not changed."
         fi
     else
-        echo "[ERROR][watchdog] Error occured when parsing response from Ngrok! See below for details..."
-        echo "[ERROR][watchdog] Parsed:"
-        echo "$parsedngk"
-        echo "[ERROR][watchdog] From JSON response:"
-        echo "$ngk"
+        $error "[ERROR][watchdog] Error occured when parsing response from Ngrok! See below for details..."
+        $error "[ERROR][watchdog] Parsed:"
+        $error "$parsedngk"
+        $error "[ERROR][watchdog] From JSON response:"
+        $error "$ngk"
+        ngkerror=1
     fi
 else
-    echo "[ERROR][watchdog] ngrok returned empty response!"
+    $error "[ERROR][watchdog] ngrok returned empty response!"
+    ngkerror=1
 fi
 }
 
@@ -50,55 +52,77 @@ if [ ! "$ltn" == "" ]; then
     if [[ "$parsedltn" == *":"* ]]; then
         if [ ! "$(cat $ltntmp)" == "$parsedltn" ]; then
             echo $parsedltn > $ltntmp
-            echo "[INFO][watchdog] !! localtonet IP or port has changed !!"
+            $info "[INFO][watchdog] !! localtonet IP or port has changed !!"
             ltnchanged=1
         else
-            echo "[INFO][watchdog] localtonet IP or port has not changed."
+            $info "[INFO][watchdog] localtonet IP or port has not changed."
         fi
     else
-        echo "[ERROR][watchdog] Error occured when parsing response from localtonet! See below for details..."
-        echo "[ERROR][watchdog] Parsed:"
-        echo "$parsedltn"
-        echo "[ERROR][watchdog] From JSON response:"
-        echo "$ltn"
+        $error "[ERROR][watchdog] Error occured when parsing response from localtonet! See below for details..."
+        $error "[ERROR][watchdog] Parsed:"
+        $error "$parsedltn"
+        $error "[ERROR][watchdog] From JSON response:"
+        $error "$ltn"
+        ltnerror=1
     fi
 else
-    echo "[ERROR][watchdog] localtonet returned empty response!"
+    $error "[ERROR][watchdog] localtonet returned empty response!"
+    ltnerror=1
 fi
 }
 
-# ngrok
+# Check ngrok if configured
 if [[ -f $AlertConf/ngk-auth ]]; then
     if pgrep -x ngrok > /dev/null
     then
-        echo "[INFO][watchdog] Getting IP from ngrok..."
+        $info "[INFO][watchdog] Getting IP from ngrok..."
         ngk=$($AlertConf/wd-ngk.sh)
         checkngk
     else
-        echo "[WARN][watchdog] ngrok is not running!"
-        ngknorun=1
+        $warn "[WARN][watchdog] ngrok is not running!"
+        ngkerror=1
     fi
 fi
 
-# localtonet
+# Check localtonet if configured
 if [[ -f $AlertConf/ltn-auth ]]; then
     if pgrep -x localtonet > /dev/null
     then
-        echo "[INFO][watchdog] Getting IP from localtonet..."
+        $info "[INFO][watchdog] Getting IP from localtonet..."
         ltn=$($AlertConf/wd-ltn.sh)
         checkltn
     else
-        echo "[WARN][watchdog] localtonet is not running!"
-        ltnnorun=1
+        $warn "[WARN][watchdog] localtonet is not running!"
+        ltnerror=1
     fi
 fi
 
-if [[ ! -z "$ngknorun" && "$ltnnorun" ]]; then
-    echo "[ERROR][watchdog] No available sources."
+if [[ ! -z "$ngkerror" && "$ltnerror" ]]; then
+    $fatal "[FATAL][watchdog] No available sources to check, or all sources returned errors. ABORT!"
     exit 1
 fi
 
-if [[ ! -z "$ngkchanged" && "$ltnchanged" ]]; then
-    echo both changed!
-    $AlertConf/alert.sh -1 "$parsedngk" -2 "$parsedltn" -c webhook
+# If both LTN and NGK are installed, but one has an error, then replace its empty parsed string with an error msg
+# The nested ifs are bad with this one, but I do not care.
+# TODO: Add support for only one being installed.
+if [[ -f $AlertConf/ngk-auth && -f $AlertConf/ltn-auth ]]; then
+
+    # Ignore offline sources
+    if [[ ! -z $ltnerror || $ngkerror ]]; then
+        parseerror="Offline"
+        if [[ -z $parsedngk ]]; then
+            parsedngk=$parseerror
+            echo $parsedngk > $ngktmp
+        fi
+        if [[ -z $parsedltn ]]; then
+            parsedltn=$parseerror
+            echo $parsedltn > $ltntmp
+        fi
+    fi
+
+    # If one or more have been changed, send an alert.
+    if [[ ! -z "$ngkchanged" || "$ltnchanged" ]]; then
+        $info "[INFO][watchdog] One or more IPs have changed!"
+        $AlertConf/alert.sh -1 "$parsedngk" -2 "$parsedltn" -c webhook
+    fi
 fi
